47513bda644f2f83be5f1da6a34e6a6622ebf4c5
pritiantala <pritiantala209@gmail.com>
2022-07-06 12:29:23 +0530

    update

---
+++ sample.py
@@ -0,0 +1,97 @@
+ What is List? How will you reverse a list?
+ How will you remove last object from a list?
+ Suppose list1 is [2, 33, 222, 14, and 25], what is list1 [-1]?
+ Differentiate between append () and extend () methods?
+ Write a Python function to get the largest number, smallest num and sum 
+of all from a list.
+ How will you compare two lists?
+ Write a Python program to count the number of strings where the string 
+length is 2 or more and the first and last character are same from a given 
+list of strings.
+ Write a Python program to remove duplicates from a list.
+ Write a Python program to check a list is empty or not.
+ Write a Python function that takes two lists and returns true if they have 
+at least one common member.
+ Write a Python program to generate and print a list of first and last 5 
+elements where the values are square of numbers between 1 and 30.
+ Write a Python function that takes a list and returns a new list with unique 
+elements of the first list.
+ Write a Python program to convert a list of characters into a string.
+ Write a Python program to select an item randomly from a list.
+ Write a Python program to find the second smallest number in a list.
+ Write a Python program to get unique values from a list
+ Write a Python program to check whether a list contains a sub list
+ Write a Python program to split a list into different variables.
+ What is tuple? Difference between list and tuple.
+ Write a Python program to create a tuple with different data types.
+ Write a Python program to create a tuple with numbers.
+ Write a Python program to convert a tuple to a string.
+ Write a Python program to check whether an element exists within a 
+tuple.
+ Write a Python program to find the length of a tuple.
+ Write a Python program to convert a list to a tuple.
+ Write a Python program to reverse a tuple.
+ Write a Python program to replace last value of tuples in a list.
+ Write a Python program to find the repeated items of a tuple.
+ Write a Python program to remove an empty tuple(s) from a list of tuples.
+ Write a Python program to unzip a list of tuples into individual lists.
+ Write a Python program to convert a list of tuples into a dictionary.
+ How will you create a dictionary using tuples in python?
+ Write a Python script to sort (ascending and descending) a dictionary by 
+value.
+ Write a Python script to concatenate following dictionaries to create a 
+new one.
+ Write a Python script to check if a given key already exists in a dictionary.
+ How Do You Traverse Through A Dictionary Object In Python?
+ How Do You Check The Presence Of A Key In A Dictionary?
+ Write a Python script to print a dictionary where the keys are numbers 
+between 1 and 15.
+ Write a Python program to check multiple keys exists in a dictionary
+ Write a Python script to merge two Python dictionaries
+ Write a Python program to map two lists into a dictionary
+ Write a Python program to combine two dictionary adding values for 
+common keys.
+o d1 = {'a': 100, 'b': 200, 'c':300}
+o d2 = {'a': 300, 'b': 200,’d’:400}
+ Sample output: Counter ({'a': 400, 'b': 400,’d’: 400, 'c': 300}).
+ Write a Python program to print all unique values in a dictionary.
+ Why Do You Use the Zip () Method in Python?
+ Write a Python program to create and display all combinations of letters,
+selecting each letter from a different key in a dictionary.
+o Sample data: {'1': ['a','b'], '2': ['c','d']}
+o Expected Output:
+o ac ad bc bd
+ Write a Python program to find the highest 3 values in a dictionary
+ Write a Python program to combine values in python list of dictionaries. 
+Sample data: [{'item': 'item1', 'amount': 400}, {'item': 'item2', 'amount': 
+300},
+o {'item': 'item1', 'amount': 750}]
+o Expected Output: Counter ({'item1': 1150, 'item2': 300})
+ Write a Python program to create a dictionary from a string.
+o Note: Track the count of the letters from the string. Sample string:
+'w3resource'
+o Expected output: {'3': 1,’s’: 1, 'r': 2, 'u': 1, 'w': 1, 'c': 1, 'e': 2, 'o': 1}
+ Write a Python function to calculate the factorial of a number (a nonnegative integer)
+ Write a Python function to check whether a number is in a given range
+ Write a Python function to check whether a number is perfect or not.
+ Write a Python function that checks whether a passed string is palindrome 
+or not
+ How do you perform pattern matching in Python? Explain
+ What is lambda function in python? What we call a function which is 
+incomplete version of a function?
+ How Many Basic Types Of Functions Are Available In Python?
+ How can you pick a random item from a list or tuple?
+ How can you pick a random item from a range?
+ How can you get a random number in python?
+ How will you set the starting value in generating random numbers?
+ How will you randomizes the items of a list in place?
+ Write a Python program to read a random line from a file.
+ Write a Python program to convert degree to radian
+ Write a Python program to calculate the area of a trapezoid
+ Write a Python program to calculate the area of a parallelogram
+ Write a Python program to calculate surface volume and area of a cylinder
+ Write a Python program to returns sum of all divisors of a number
+ Write a Python program to find the maximum and minimum numbers 
+from the specified decimal numbers.
+Module – 4
+ What is File function in python? What is keywords to create and write fil
